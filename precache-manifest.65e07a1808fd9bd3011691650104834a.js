13f312b6a30850092f1d6a283c096aba46bd099c
Umesh Rai <Umesh.Rai508@outlook.com>
2020-09-01 16:36:21 +0100

    initial commit after the build is copied.

---
+++ precache-manifest.65e07a1808fd9bd3011691650104834a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ab3072365f38416bd9824af010bb2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cb8b7c2cdbc3123e339",
+    "url": "/static/css/main.65c2c83c.chunk.css"
+  },
+  {
+    "revision": "fe05068a4af9671d7e93",
+    "url": "/static/js/2.637f2eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637f2eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cb8b7c2cdbc3123e339",
+    "url": "/static/js/main.a5ca2f17.chunk.js"
+  },
+  {
+    "revision": "990cdd4031d1cb50819d",
+    "url": "/static/js/runtime-main.73586855.js"
+  },
+  {
+    "revision": "cd76c4529b5885a2e440d017fab0b493",
+    "url": "/static/media/LogoTnC.cd76c452.png"
+  },
+  {
+    "revision": "9dcaa121bd11b938202eb434c3dd88e5",
+    "url": "/static/media/SectionGraphics.9dcaa121.svg"
+  },
+  {
+    "revision": "f43b14d8b084715693a7e0e1966b281d",
+    "url": "/static/media/coffeeshop_index2.f43b14d8.csv"
+  },
+  {
+    "revision": "373154a0870ed0efe10dd07740ae9446",
+    "url": "/static/media/home.373154a0.svg"
+  },
+  {
+    "revision": "282c090d0dec157f87a8b65266ba905e",
+    "url": "/static/media/icon_costa.282c090d.svg"
+  },
+  {
+    "revision": "955db20431dbb1ccd75c8640e1c6b518",
+    "url": "/static/media/icon_esquire.955db204.svg"
+  },
+  {
+    "revision": "26e8e6f6e287c7fbc77193acf308385d",
+    "url": "/static/media/icon_greggs.26e8e6f6.svg"
+  },
+  {
+    "revision": "296a0312d5a28665bd55510e35b94daa",
+    "url": "/static/media/icon_muffin.296a0312.svg"
+  },
+  {
+    "revision": "ba474c0729cef4baadc6ee570310dab2",
+    "url": "/static/media/icon_nero.ba474c07.svg"
+  },
+  {
+    "revision": "b2610c895228f936f1e986f463ea724a",
+    "url": "/static/media/icon_pret.b2610c89.svg"
+  },
+  {
+    "revision": "70f59637ee2fa8eaf92472c88f68d232",
+    "url": "/static/media/icon_starbuck.70f59637.svg"
+  },
+  {
+    "revision": "b7f2f06d1e48a85578e29fc5bcea5992",
+    "url": "/static/media/select-appreance.b7f2f06d.svg"
+  }
+]);
